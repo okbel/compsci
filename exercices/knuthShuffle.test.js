0dcb6b55f86f6b4eae6fa7ee565aeb5d7b12a451
Belen Curcio <curciobelen@gmail.com>
2017-05-29 00:06:49 -0300

    Moving to folders

--- exercices/knuthShuffle.test.js
+++ exercices/knuthShuffle.test.js
@@ -8,7 +8,7 @@ function knuthShuffle(init) {
   }
 
   function randomIdx(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min
+    return Math.floor(Math.random() * (max - min + 1)) + min
   }
 
   for (let i = 0; i < arr.length; i++) {
